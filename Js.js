f367a623577a1cbe7bb9f29e4d5daf9b3eeb5669
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2022-03-09 18:30:52 +0530

    Add files via upload
    Added all files from local storage

---
+++ Js.js
@@ -0,0 +1,6 @@
+var firstName = "Sandeep";
+var lastName = ".K"
+var username = firstName + lastName;
+
+
+document.getElementById("Creator").innerHTML = username;
---
+++ index.js
@@ -0,0 +1,20 @@
+var users = [ "Sandeep", "Konda Sandeep"];
+
+let username = document.getElementById("username");
+let email = document.getElementById("emailId");
+    
+function note() {
+    let name = username.value, email = email.value; 
+    
+  //  if(username === users[0] || username === users[1]) {
+    
+    document.getElementById"message").value = "Welcome To My Webpage"; 
+    document.getElementById("link").innerHTML = name; 
+    document.getElementById("click").style.placeholder = "Click on Your Name!";
+   /* }
+    else { 
+        alert("failed Authentication");
+        console.log("hi");
+    }*/
+    console.log("bye");
+}
